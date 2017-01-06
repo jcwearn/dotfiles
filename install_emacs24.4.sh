98a8ee6be42cbd16a53e3826c3581a21a3fd9cec
Jackson Wearn <jacksonwearn@gmail.com>
2017-01-06 11:40:46 -0500

    Breakout logic into functions.  Add function to backup existing config

--- install_emacs24.4.sh
+++ install_emacs24.4.sh
@@ -1,25 +1,67 @@
 #!/bin/bash
-cd ~
 
-#install emacs24.4
 platform=$(uname)
+emacs_path=$(which emacs)
 
-if [[ $platform == "Darwin" ]]; then
-    brew install emacs --with-cocoa --with-gnutls
-elif [[ $platform == "Linux"  ]]; then
-    taken from http://askubuntu.com/a/546050
-    mkdir emacs-src && cd emacs-src
-    wget http://mirror.team-cymru.org/gnu/emacs/emacs-24.4.tar.gz
-    tar xvf emacs-24.4.tar.gz
+check_emacs_installed() {
+    if [ "$emacs_path" != "" ]; then
+	emacs_installed=true
+    else
+	emacs_installed=false
+    fi
+}
 
-     sudo apt-get install build-essential
-     sudo apt-get build-dep emacs24
-     cd emacs-24.4
-     ./configure
-     make
-     sudo make install
-     rm -rf ~/emacs-src
+find_emacs_version() {
+    emacs_version=$(emacs --version | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")
+    emacs_version=${emacs_version:0:4}
+}
+
+install_emacs() {
+    if [[ $platform == "Darwin" ]]; then
+        brew install emacs --with-cocoa --with-gnutls
+    elif [[ $platform == "Linux"  ]]; then
+	#taken from http://askubuntu.com/a/546050
+	mkdir emacs-src && cd emacs-src
+	wget http://mirror.team-cymru.org/gnu/emacs/emacs-24.4.tar.gz
+        tar xvf emacs-24.4.tar.gz
+
+        sudo apt-get install build-essential
+        sudo apt-get build-dep emacs24
+        cd emacs-24.4
+        ./configure
+        make
+        sudo make install
+        rm -rf ~/emacs-src
+    fi
+}
+
+backup_existing_emacs_config() {
+    date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    backup_directory=~/emacs_backup-${date}
+    echo ${backup_directory}
+    mkdir ${backup_directory}
+    cp ~/.emacs ${backup_directory}
+    cp -r ~/.emacs.d/ ${backup_directory}/.emacs.d/
+}
+
+install_emacs_config() {
+    echo "Installing dotfiles"
+    cp .emacs ~/.emacs
+    cp -r .emacs.d/ ~/emacs.d/
+}
+
+check_emacs_installed
+if [ $emacs_installed ]; then
+    find_emacs_version
+    minimum_version="24.4"
+    if [ "$emacs_version" > "$minimum_version" ]; then
+	backup_existing_emacs_config
+	install_emacs_config
+    else
+	echo "The version of Emacs installed on this system is not supported."
+	echo  "Please install Emacs version 24.4 or greater."
+    fi
+else
+    install_emacs
+    install_emacs_config
 fi
-
-cp ~/dotfiles/.emacs
-cp -r ~/dotfiles/.emacs.d/
